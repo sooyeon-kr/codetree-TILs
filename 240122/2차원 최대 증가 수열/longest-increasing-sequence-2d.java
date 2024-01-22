b95530abefdb9fd22c4e1761b85d0f35ade41f1d
SooyeonOh <39377091+sooyeon-kr@users.noreply.github.com>
2024-01-22 17:44:44 +0900

    Title: 2차원 최대 증가 수열, Time: 128ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240122/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[][] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        dp = new int[N][M];
+        for(int y=0; y<N; ++y){
+            st = new StringTokenizer(br.readLine());
+            for(int x=0; x<M; ++x){
+                arr[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int ans = 0;
+
+        dp[0][0] = 1;
+        for(int y=0; y<N; ++y){
+            for(int x=0; x<M; ++x){
+                dp[y][x] = findMaxNum(arr[y][x], y, x);
+                ans = Math.max(ans, dp[y][x]);
+            }
+        }
+
+
+        System.out.println(ans);
+    
+    }
+
+    static int findMaxNum(int max, int Y, int X){
+
+        int ret = 1;
+
+        for(int y=0; y<Y; ++y){
+            for(int x=0; x<Y; ++x){
+                if(arr[y][x] >= max) continue;
+                ret = Math.max(ret, dp[y][x]+1);
+            }
+        }
+
+        return ret;
+    }
+
+}
