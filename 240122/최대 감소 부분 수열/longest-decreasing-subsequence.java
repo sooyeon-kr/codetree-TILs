6b8a485c25c242dc37695b5d67b08e6a1cf60e4e
SooyeonOh <39377091+sooyeon-kr@users.noreply.github.com>
2024-01-22 15:42:43 +0900

    Title: 최대 감소 부분 수열, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240122/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 240122/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -6,27 +6,29 @@ public class Main {
     static int[] nums;
     static int[] dp;
     public static void main(String[] args) throws Exception{
+    	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = null;
-
         N = Integer.parseInt(br.readLine());
-        nums = new int[N];
-        dp = new int[N];
+        nums = new int[N+1];
+        dp = new int[N+1];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; ++i)
-            nums[i] = Integer.parseInt(st.nextToken());
+            nums[i+1] = Integer.parseInt(st.nextToken());
         
-        dp[0] = 1;
+        dp[0] = 0;
         for(int i=1; i<N; ++i){
-            for(int j=0; j<i; ++j){
+        	if(dp[i] == 0) dp[i] = 1;
+            for(int j=1; j<i; ++j){
                 if(nums[j] <= nums[i]) continue;
                 dp[i] = Math.max(dp[i], dp[j]+1);
             }
         }
         int ans = 0;
+        
         for(int i=0; i<N; ++i)
             ans = Math.max(ans, dp[i]);
 
