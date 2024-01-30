ba709311e8e814fc8e31621ee614f715acf5b3b4
SooyeonOh <39377091+sooyeon-kr@users.noreply.github.com>
2024-01-30 17:50:47 +0900

    Title: 계단 오르기 2, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

--- 240130/계단 오르기 2/climbing-stairs-2.java
+++ 240130/계단 오르기 2/climbing-stairs-2.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int INF = (int)1e6;
     static int N;
     static int[] nums;
     static int[][] dp;
@@ -18,9 +19,18 @@ public class Main {
         String[] str = br.readLine().split(" ");
         for(int i=1; i<=N; ++i) nums[i] = Integer.parseInt(str[i-1]);
 
+        dp[1][0] = -INF;
+        dp[1][2] = -INF;
+        dp[1][3] = -INF;
+
+        dp[2][1] = -INF;
+        dp[2][3] = -INF;
+
+
         dp[1][1] = nums[1];
         dp[2][0] = nums[2];
         dp[2][2] = nums[1]+nums[2];
+        
         for(int i=3; i<=N; ++i){
             for(int j=0; j<4; ++j){
                 dp[i][j] = Math.max(dp[i][j], dp[i-2][j] + nums[i]);
