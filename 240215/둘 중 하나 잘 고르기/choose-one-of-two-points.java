bd81a1944a50344cd74c1a710ae09a75586f1402
SooyeonOh <39377091+sooyeon-kr@users.noreply.github.com>
2024-02-15 10:22:18 +0900

    Title: 둘 중 하나 잘 고르기, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240215/둘 중 하나 잘 고르기/choose-one-of-two-points.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MN = 105;
+    static int[] red = new int[MN*2], blue = new int[MN*2];
+    static int N;
+    static int[][] dp = new int[MN*2][MN];
+    static int ans = 0;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+        for(int i=1; i<=N; ++i){
+            String[] input = br.readLine().split(" ");
+            red[i] = Integer.parseInt(input[0]);
+            blue[i] = Integer.parseInt(input[1]);
+        }
+
+        dp[0][0] = 0;
+        for(int i=1; i<=2*N; ++i){
+            for(int j=1; j<=2*N; ++j){
+                if(j <= N){
+                    dp[i][j] = Math.max(dp[i-1][j-1]+red[i], dp[i-1][j]);
+                }
+                dp[i][j] = Math.max(dp[i-1][j] + blue[i],dp[i][j]);
+                
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        for(int i=0; i<=2*N; ++i){
+            for(int j=0; j<=2*N; ++j){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println(ans);
+    }
+}
