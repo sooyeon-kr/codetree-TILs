e819cbce6168b18ad3b4f7d9dbef8f2415d603b2
SooyeonOh <sooyeon7027@naver.com>
2024-11-14 15:22:43 +0900

    Title: 선분 위의 점, Time: 284ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241114/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; ++i){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<M; ++i){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            System.out.println(upper(arr, e) - lower(arr, s));
+        }
+    }
+
+    static int lower(int[] arr, int target){
+        int st = 0, en = arr.length -1;
+        int maxIdx = en + 1;
+
+        while(st <= en){
+            int mid = (st+en) / 2;
+
+            if(arr[mid] >= target){
+                en = mid - 1;
+                maxIdx = Math.min(maxIdx, mid);
+            }
+
+            else{
+                st = mid + 1;
+            }
+        }
+
+        return maxIdx;
+    }
+
+    static int upper(int[] arr, int target){
+        int st = 0, en = arr.length -1;
+        int maxIdx = en + 1;
+
+        while(st <= en){
+            int mid = (st+en) / 2;
+
+            if(arr[mid] > target){
+                en = mid - 1;
+                maxIdx = Math.min(maxIdx, mid);
+            }
+
+            else{
+                st = mid + 1;
+            }
+        }
+
+        return maxIdx;
+    }
+}
