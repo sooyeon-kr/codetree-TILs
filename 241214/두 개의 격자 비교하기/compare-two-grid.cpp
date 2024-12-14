df7b68bb701dd949867332ab5fb4f82e8b31debf
SooyeonOh <sooyeon7027@naver.com>
2024-12-14 20:25:23 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241214/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 241214/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 #define N 10
 #define M 10
+void input(int arr[][M], int n, int m){
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<m; ++x){
+            cin>>arr[y][x];
+        }
+    }
+}
 int main() {
     // 여기에 코드를 작성해주세요.
     int pre[N][M]={}, after[N][M]={}, ans[N][M] = {};
@@ -9,18 +16,19 @@ int main() {
 
     cin >> n >> m;
 
-    for(int y=0; y<n; ++y){
-        for(int x=0; x<m; ++x){
-            cin>>pre[y][x];
-        }
-    }
+    // for(int y=0; y<n; ++y){
+    //     for(int x=0; x<m; ++x){
+    //         cin>>pre[y][x];
+    //     }
+    // }
 
-    for(int y=0; y<n; ++y){
-        for(int x=0; x<m; ++x){
-            cin>>after[y][x];
-        }
-    }
-
+    // for(int y=0; y<n; ++y){
+    //     for(int x=0; x<m; ++x){
+    //         cin>>after[y][x];
+    //     }
+    // }
+    input(pre, n, m);
+    input(after, n, m);
     for(int y=0; y<n; ++y){
         for(int x=0; x<m; ++x){
             if(pre[y][x] != after[y][x]){
@@ -38,3 +46,4 @@ int main() {
     
     return 0;
 }
+
