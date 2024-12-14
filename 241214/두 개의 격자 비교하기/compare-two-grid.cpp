43d558a00a151321ca81e97c0744d1508b2676b5
SooyeonOh <sooyeon7027@naver.com>
2024-12-14 20:18:16 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241214/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#define N 10
+#define M 10
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int pre[N][M]={}, after[N][M]={}, ans[N][M] = {};
+    int n, m;
+
+    cin >> n >> m;
+
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<m; ++x){
+            cin>>pre[y][x];
+        }
+    }
+
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<m; ++x){
+            cin>>after[y][x];
+        }
+    }
+
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<m; ++x){
+            if(pre[y][x] != after[y][x]){
+                ans[y][x] = 1;
+            }
+        }
+    }
+    
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<m; ++x){
+            cout<<ans[y][x]<<' ';
+        }
+        cout<<'\n';
+    }
+    
+    return 0;
+}
